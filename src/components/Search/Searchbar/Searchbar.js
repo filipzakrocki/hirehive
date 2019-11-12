a8bbe715cdc95c3f3a333b57fcf433857858fc20
filipzakrocki <42967255+filipzakrocki@users.noreply.github.com>
2019-11-12 21:17:56 +0000

    added icon

--- src/components/Search/Searchbar/Searchbar.js
+++ src/components/Search/Searchbar/Searchbar.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './Searchbar.css'
-import search from url("//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css");
+import {searchIcon} from '../../../assets/search-solid.png'
 
 import { url } from 'inspector';
 
 const Searchbar = (props) => {
     return (
         <div className='Searchbar'>
-            <div className=''>
-
-            </div>
             <input
             className='Searchbar-input'
             type="text"
