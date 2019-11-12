39f41e02d582a7118e27edc39030efda95971627
Filip <42967255+filipzakrocki@users.noreply.github.com>
2019-11-12 19:45:29 +0000

    Update Search.js

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -7,12 +7,12 @@ import React from "react";
 const Search = props => {
   // mapping unique categories to display as buttons
   const categories = props.jobs ? props.jobs.map(el => el.category) : null;
-  let categoryButtons = categories
+  const categoryButtons = categories
     ? [...new Set(categories)].map(uniqueCategory => {
         return (
           <button
             key={uniqueCategory}
-            active={props.category === uniqueCategory}
+            active="true"
             onClick={() => props.categoryChangeHandler(uniqueCategory)}
           >
             #{uniqueCategory.toUpperCase()}
@@ -27,7 +27,7 @@ const Search = props => {
         type="text"
         onChange={e => props.filterChangeHandler(e.target.value)}
       ></input>
-      {categoryButtons}
+      <div>{categoryButtons}</div>
     </div>
   );
 };
