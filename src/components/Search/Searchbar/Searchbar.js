79440be2bc2364f6bf5cc366ed58325848a778eb
filipzakrocki <42967255+filipzakrocki@users.noreply.github.com>
2019-11-13 01:58:49 +0000

    corrected the searchbar misalignment

--- src/components/Search/Searchbar/Searchbar.js
+++ src/components/Search/Searchbar/Searchbar.js
@@ -9,14 +9,18 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const Searchbar = (props) => {
     return (
-        <div className='Searchbar'>
-            <span className={'Searchbar-glass'}><FontAwesomeIcon icon={faSearch}  color='black'/></span>
+        <div className='Searchbar__wrapper'>
+            <div className={'Searchbar-glass'}>
+                <FontAwesomeIcon icon={faSearch}  color='black'/>
+            </div>
             <input
             disabled={props.disabled}
             className='Searchbar-input'
             type="text"
             placeholder={'Search by keyword, technology or job title'}
-            onChange={props.changed}/>
+            onChange={props.changed}>
+            </input>
+
         </div>
     );
 }
