7418be5485c26410e138593994e00f1ffa8dab61
Filip <42967255+filipzakrocki@users.noreply.github.com>
2019-11-13 16:19:15 +0000

    Update Searchbar.js

--- src/components/Search/Searchbar/Searchbar.js
+++ src/components/Search/Searchbar/Searchbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Searchbar.css";
 
-//font awesome
+//fontawesome for icon
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
