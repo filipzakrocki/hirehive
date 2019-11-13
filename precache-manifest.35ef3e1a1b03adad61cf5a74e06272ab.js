0e77be60ea49db56248da714239f1d0186a06425
Filip <42967255+filipzakrocki@users.noreply.github.com>
2019-11-13 15:48:37 +0000

    static website

---
+++ precache-manifest.35ef3e1a1b03adad61cf5a74e06272ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f6bff1eb06d8c9418b0b376330ec29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a1f9d2bc2d41a5f6d08",
+    "url": "/static/css/main.bb0c6c56.chunk.css"
+  },
+  {
+    "revision": "7c53f5a7b3fb42bc2d9c",
+    "url": "/static/js/2.f45824bf.chunk.js"
+  },
+  {
+    "revision": "9a1f9d2bc2d41a5f6d08",
+    "url": "/static/js/main.9cc38b69.chunk.js"
+  },
+  {
+    "revision": "488f7ca68a66012575e2",
+    "url": "/static/js/runtime-main.611713e8.js"
+  }
+]);
--- src/components/UI/Spinner/Spinner.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./Spinner.css";
-
-const spinner = () =>  <img alt={'spinner'} className='rotate' src={'https://hirehive.com/wp-content/uploads/2018/05/cropped-favicon-512-400x400.png'}/>
-
-export default spinner;
