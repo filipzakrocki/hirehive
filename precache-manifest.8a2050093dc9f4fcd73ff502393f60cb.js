c87bdcde662d1b1c908d7fcd7e4620f04b1443a5
Filip <42967255+filipzakrocki@users.noreply.github.com>
2019-11-13 19:10:29 +0000

    new build

--- precache-manifest.8a2050093dc9f4fcd73ff502393f60cb.js
+++ precache-manifest.8a2050093dc9f4fcd73ff502393f60cb.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f31a0c6443d5a56f287b9e0bb08cfd1c",
+    "revision": "d622cfaf6e8fed283309334ebc3ed1ec",
     "url": "./index.html"
   },
   {
-    "revision": "75d8299c1cb8a9d6c0a1",
-    "url": "./static/css/main.fbc7c3f9.chunk.css"
+    "revision": "e0c9cf0cdebab6475974",
+    "url": "./static/css/main.49f16736.chunk.css"
   },
   {
-    "revision": "7c53f5a7b3fb42bc2d9c",
-    "url": "./static/js/2.f45824bf.chunk.js"
+    "revision": "b6988ab7685b33f29832",
+    "url": "./static/js/2.4d24b2b4.chunk.js"
   },
   {
-    "revision": "75d8299c1cb8a9d6c0a1",
+    "revision": "e0c9cf0cdebab6475974",
     "url": "./static/js/main.9d49c896.chunk.js"
   },
   {
