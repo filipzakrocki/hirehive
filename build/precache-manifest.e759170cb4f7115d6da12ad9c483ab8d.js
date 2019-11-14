d5bb9f78b7421130802fc8113ad6b1211e9d3d24
Filip <42967255+filipzakrocki@users.noreply.github.com>
2019-11-14 15:30:33 +0000

    Checking out Netlify auto deployment

---
+++ build/precache-manifest.e759170cb4f7115d6da12ad9c483ab8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76bacb628d010e3c0e3dd8c65430582f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "09a0b47357854feeda5f",
+    "url": "./static/css/main.1bb29762.chunk.css"
+  },
+  {
+    "revision": "b6988ab7685b33f29832",
+    "url": "./static/js/2.4d24b2b4.chunk.js"
+  },
+  {
+    "revision": "09a0b47357854feeda5f",
+    "url": "./static/js/main.11502333.chunk.js"
+  },
+  {
+    "revision": "9d65ac25574654403922",
+    "url": "./static/js/runtime-main.3750a990.js"
+  }
+]);
